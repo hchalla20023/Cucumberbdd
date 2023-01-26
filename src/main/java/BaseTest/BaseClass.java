46d4afc8e24ecbe582858d2a870b6d379de586f5
hchalla20023 <123262612+hchalla20023@users.noreply.github.com>
2023-01-26 21:47:11 +0530

    Update BaseClass.java

--- src/main/java/BaseTest/BaseClass.java
+++ src/main/java/BaseTest/BaseClass.java
@@ -4,7 +4,7 @@ public class BaseClass {
 
     public static void main(String[] args) {
 
-        System.out.println("This base class2023");
+        System.out.println("This base class2024");
     }
 
 }
